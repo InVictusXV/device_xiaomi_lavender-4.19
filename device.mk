88be4ad7b23a2d3f37bc009c5938759e7ea571b6
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    lavender: Build common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- device.mk
+++ device.mk
@@ -465,6 +465,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator@1.2-service
