554cfdc3fdcdcf2038e2576d93a0df8c063a1bfe
InVictusXV <aradheakiki123@gmail.com>
2021-04-12 10:19:19 +0000

    lavender: remove duplicate dalvik props

--- properties.mk
+++ properties.mk
@@ -131,16 +131,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
-    dalvik.vm.dex2oat64.enabled=true
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
